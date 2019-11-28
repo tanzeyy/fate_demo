424a5f91377a60432c5b6aba643fb4c05630bc19
CuiSir <1174988626@qq.com>
2019-11-28 21:30:45 +0800

    csv

--- client/application/api.py
+++ client/application/api.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, jsonify, make_response
+from flask import Blueprint, jsonify, make_response, request, current_app
+from .utils import ok_response, error_response
+from .db import get_db
+import time
+import csv
 
 bp = Blueprint('data', __name__, url_prefix='/data')
 
@@ -11,17 +15,40 @@ def read_data():
 
     # 没有查询的sql语句
     if not request.data:
-        return
+        return error_response(message="None data.")
     data = request.get_json()
+
     sql = data.get('data_sql')
+    # 没有sql语句
     if not sql:
+        return error_response(message="None data_sql in request.")
 
+    db = get_db()
+    db_data = None
+    try:
+        db_data = db.execute(r'''{}'''.format(sql)).fetchall()
+    except Exception as e:
+        return error_response(message="Query data from databse error. Error info: " + str(e))
 
+    if (len(db_data) == 0):
+        return error_response(message="没有满足条件的数据")
 
+    table_head = len(db_data[0])
+    if (table_head < 2):
+        return error_response(message="数据缺少id或者标签")
+    head = ['x' + str(i) for i in range(table_head-2)]
 
+    file_path = current_app.config['TEMP_DATA_DIR']
+    current_milli_time = lambda: str(round(time.time() * 1000))
+    file_path = file_path + current_milli_time() + '.csv'
 
-    return make_response(jsonify(code=200, message='fate demo'), 200)
+    with open(file_path, "w") as csvfile:
+        writer = csv.writer(csvfile)
+        # 先写入columns_name
+        row = ["x", "y"]
+        row.extend(head)
+        writer.writerow(row)
+        # 写入多行用writerows
+        writer.writerows(db_data)
 
-@bp.route('/train/generator', methods=['POST'])
-def generator():
-    pass
+    return ok_response(message="ok")
--- client/instance/config.py
+++ client/instance/config.py
@@ -1,1 +1,6 @@
+import os
+
 SQLALCHEMY_DATABASE_URI="mysql+pymysql://fl:123@192.168.23.103:3306/data"
+
+INSTANCE_DIR = os.path.split(os.path.realpath(__file__))[0]
+TEMP_DATA_DIR = INSTANCE_DIR + "/data/"
