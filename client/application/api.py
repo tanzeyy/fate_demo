4583c18b69438fc8674c9cce82b24bb55de31014
CuiSir <1174988626@qq.com>
2019-11-28 19:42:18 +0800

    tmp

--- client/application/api.py
+++ client/application/api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, make_response
+from flask import Blueprint, jsonify, make_response, request
 
 bp = Blueprint('data', __name__, url_prefix='/data')
 
@@ -8,6 +8,16 @@ def read_data():
     根据sql语句读取数据
     生成csv
     '''
+
+    # 没有查询的sql语句
+    if not request.data:
+        return 
+    data = request.get_json()
+    sql = data.get('data_sql')
+    if not sql:
+
+
+
 
 
     return make_response(jsonify(code=200, message='fate demo'), 200)
