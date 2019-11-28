d0421147122d40686f4f701f3a88896fff984ff3
CuiSir <1174988626@qq.com>
2019-11-28 19:31:07 +0800

    client

--- client/application/api.py
+++ client/application/api.py
@@ -1,9 +1,15 @@
 from flask import Blueprint, jsonify, make_response
 
-bp = Blueprint('api', __name__, url_prefix='/api')
+bp = Blueprint('data', __name__, url_prefix='/data')
 
-@bp.route('/test', methods=['GET', ])
-def test():
+@bp.route('/read_data', methods=['POST'])
+def read_data():
+    '''
+    根据sql语句读取数据
+    生成csv
+    '''
+
+    
     return make_response(jsonify(code=200, message='fate demo'), 200)
 
 @bp.route('/train/generator', methods=['POST'])
---
+++ client/instance/config.py
@@ -0,0 +1,1 @@
+SQLALCHEMY_DATABASE_URI="mysql+pymysql://fl:123@192.168.23.103:3306/data"
