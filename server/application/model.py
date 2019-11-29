ca9b7310601360f3747cdc250a4224133badb32f
CuiSir <1174988626@qq.com>
2019-11-29 18:42:52 +0800

    train json

--- server/application/model.py
+++ server/application/model.py
@@ -5,6 +5,7 @@ from flask import Blueprint, jsonify, make_response, request, current_app
 from .libs.utils import ok_response, error_response
 from .libs.db import get_db
 from .libs.models import User, Model, Order
+import json
 
 bp = Blueprint('model', __name__, url_prefix='/model')
 
@@ -34,11 +35,28 @@ def model_train():
     p.close()
     p.join()
 
+    initiator = db.query(User).filter(User.id == user_id).first()
+
     # Generate config file
-    
+    TRAIN_TEMPLATE = current_app.config['TRAIN_TEMPLATE']
+    with open(TRAIN_TEMPLATE, 'r', encoding='utf-8') as f:
+        conf_json = json.loads(f.read())
+
+        conf_json['initiator']['party_id'] = initiator.party_id
+        conf_json['initiator']['role'] = 'guest'
+
+        conf_json['role']['guest'] = []
+        conf_json['role']['host'] = []
+        conf_json['role']['arbiter'] = []
+
+        conf_json['role_parameters']['guest']['args']['data']['train_data'] = []
+        conf_json['role_parameters']['host']['args']['data']['train_data'] = []
+
+        conf_json['algorithm_parameters']['homo_lr_0'] = model_param
+
+    # post json
+    # TODO: 加API
+    requests.post(initiator.client_url, json=data)
 
 
-
-
-    
     return make_response(jsonify(code=200, message='fate demo'), 200)
--- server/instance/config.py
+++ server/instance/config.py
@@ -1,2 +1,5 @@
+import os
 SQLALCHEMY_DATABASE_URI="mysql+pymysql://fl:123@192.168.23.103:3306/app"
 SUBMIT_DATA_URI="{}/data/read_data"
+INSTANCE_DIR = os.path.split(os.path.realpath(__file__))[0]
+TRAIN_TEMPLATE = INSTANCE_DIR + "/../application/templates/train_homo_lr_job_conf.json"
