a578eaf070a468dbaef611c4acf89d236a8a08cf
tanzeyy <tan_ze@outlook.com>
2019-12-02 16:07:20 +0800

    update run.sh

--- server/run.sh
+++ server/run.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+export LC_ALL=zh_CN.utf8
+export LANG=zh_CN.utf8
 export FLASK_APP="application"
 export FLASK_ENV="development"
-python -m flask run --host=0.0.0.0
+flask run --no-reload --host=0.0.0.0
